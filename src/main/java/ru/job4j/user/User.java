bcd0b38b1a003bd122cf8eed89ffeaf4fe0537fe
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-11-12 17:45:19 +0300

    3. Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/user/User.java
+++ src/main/java/ru/job4j/user/User.java
@@ -1,6 +1,5 @@
 package ru.job4j.user;
 
-import javax.xml.crypto.Data;
 import java.util.*;
 
 public class User {
@@ -22,7 +21,7 @@ public class User {
                 + ", birthday=" + birthday
                 + '}';
     }
-//
+
 //    @Override
 //    public boolean equals(Object o) {
 //        if (this == o) {
@@ -34,24 +33,29 @@ public class User {
 //        User user = (User) o;
 //        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
 //    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(name, children, birthday);
-//    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
 
     public static void main(String[] args) {
+        Map<User, Object> map = new HashMap<>();
         User user = new User("Dastan", 2, new GregorianCalendar(2021, 3, 23));
         User user1 = new User("Ivan", 3, new GregorianCalendar(2022, 4, 27));
-        Map<User, Object> map = new HashMap<>();
+        User user2 = new User("Dania", 1, new GregorianCalendar(2024, 1, 20));
         map.put(user, "first");
         map.put(user1, "second");
+        map.put(user2, "third");
         System.out.println(map);
-        User user2 = new User("Dastan", 2, new GregorianCalendar(2021, 3, 23));
-        map.put(user2, "first");
-        System.out.println(map);
+
+        User user3 = new User("Dastan", 2, new GregorianCalendar(2021, 3, 23));
+        map.put(user3, "first");
+        boolean result = map.containsKey(user3);
+        System.out.println("result : " + result);
+        System.out.println(user.equals(user3));
         System.out.println(user.hashCode());
-        System.out.println(user2.hashCode());
-        System.out.println(user1.hashCode());
+        System.out.println(user3.hashCode());
+        System.out.println(map);
     }
 }
