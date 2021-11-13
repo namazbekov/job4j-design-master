4fe1af546bca30944ecaa01dd4f0c00f44cbaa34
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-11-13 15:36:48 +0300

    fix FlatMapTest

--- src/test/java/ru/job4j/iterator/FlatMapTest.java
+++ src/test/java/ru/job4j/iterator/FlatMapTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.hamcrest.Matchers.is;
 import org.junit.Test;
+
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -56,7 +58,7 @@ public class FlatMapTest {
     @Test(expected = NoSuchElementException.class)
     public void whenEmpty() {
         Iterator<Iterator<Object>> data = List.of(
-                List.of().iterator()
+                Collections.emptyIterator()
         ).iterator();
         FlatMap<Object> flat = new FlatMap<>(data);
         flat.next();
@@ -65,9 +67,9 @@ public class FlatMapTest {
     @Test
     public void whenSeveralEmptyAndNotEmpty() {
         Iterator<Iterator<?>> it = List.of(
-                List.of().iterator(),
-                List.of().iterator(),
-                List.of().iterator(),
+                Collections.emptyIterator(),
+                Collections.emptyIterator(),
+                Collections.emptyIterator(),
                 List.of(1).iterator()
         ).iterator();
         FlatMap flat = new FlatMap(it);
@@ -78,10 +80,10 @@ public class FlatMapTest {
     @Test
     public void whenSeveralEmptyThenReturnFalse() {
         Iterator<Iterator<Object>> it = List.of(
-                List.of().iterator(),
-                List.of().iterator(),
-                List.of().iterator(),
-                List.of().iterator()
+                Collections.emptyIterator(),
+                Collections.emptyIterator(),
+                Collections.emptyIterator(),
+                Collections.emptyIterator()
         ).iterator();
         FlatMap flat = new FlatMap(it);
         assertFalse(flat.hasNext());
