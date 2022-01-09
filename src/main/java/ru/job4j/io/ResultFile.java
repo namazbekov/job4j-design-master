99f5e34f495f09742a47a6cb035f42b853b36ee2
namazbekov <82052352+namazbekov@users.noreply.github.com>
2022-01-09 15:26:24 +0300

    0.1. FileOutputStream. [#252488]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -5,21 +5,23 @@ import java.io.*;
 
 
 public class ResultFile {
-    public static void write(String filename, int[][]x) throws IOException {
-        BufferedWriter outputWriter;
-        outputWriter = new BufferedWriter(new FileWriter(filename));
-        for (int[] ints : x) {
-            for (int j = 0; j < x.length; j++) {
-                outputWriter.write(ints[j] + "");
+    public static void write(String filename, int[][]x) {
+        File file = new File(filename);
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+            for (int i = 0; i < x.length; i++) {
+                for (int j = 0; j < x.length; j++) {
+                    String arr = Integer.toString(x[i][j]);
+                    fileOutputStream.write(Byte.parseByte(arr + ""));
 
-                outputWriter.newLine();
+                    fileOutputStream.write(System.lineSeparator().getBytes());
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        outputWriter.flush();
-        outputWriter.close();
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         int[][] array = new int[5][5];
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
