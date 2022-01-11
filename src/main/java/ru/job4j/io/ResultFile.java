b70e1c61395e54e35f24d22ef72268c2428b0b08
namazbekov <82052352+namazbekov@users.noreply.github.com>
2022-01-11 16:54:07 +0300

    0.1. FileOutputStream. [#252488]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -5,14 +5,13 @@ import java.io.*;
 
 
 public class ResultFile {
-    public static void write(String filename, int[][]x) {
+    public void write(String filename, int[][]x) {
         File file = new File(filename);
         try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
             for (int i = 0; i < x.length; i++) {
                 for (int j = 0; j < x.length; j++) {
                     String arr = Integer.toString(x[i][j]);
-                    fileOutputStream.write(Byte.parseByte(arr + ""));
-
+                    fileOutputStream.write(arr.getBytes());
                     fileOutputStream.write(System.lineSeparator().getBytes());
                 }
             }
@@ -28,7 +27,7 @@ public class ResultFile {
                 array[i][j] = (i + 1) * (j + 1);
             }
         }
-
-        ResultFile.write("result.txt", array);
+        ResultFile finalResult = new ResultFile();
+        finalResult.write("result.txt", array);
     }
 }
