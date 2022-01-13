f86b325376744e067b1745942d94247f095299a6
namazbekov <82052352+namazbekov@users.noreply.github.com>
2022-01-13 12:18:08 +0300

    0.3. BufferedReader. [#252489]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,29 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class LogFilter {
+    public static List<String> filter(String file) {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            list = in.lines().map(String :: toLowerCase)
+                    .filter(s -> s.startsWith("404"))
+                    .collect(Collectors.toCollection(ArrayList :: new));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        System.out.println(log);
+    }
+}
---
+++ src/main/java/ru/job4j/io/ReadFile2.java
@@ -0,0 +1,13 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class ReadFile2 {
+    public static void main(String[] args) {
+        try (BufferedReader in = new BufferedReader(new FileReader("input.txt"))) {
+            in.lines().forEach(System.out :: println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
