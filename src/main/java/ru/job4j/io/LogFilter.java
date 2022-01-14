77605f7864d573df0ff95fac1f4e3716b6798aa5
namazbekov <82052352+namazbekov@users.noreply.github.com>
2022-01-15 02:46:43 +0300

    0.3. BufferedReader. [#252489 #250302]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import com.sun.jdi.request.StepRequest;
 import ru.job4j.list.SimpleArrayList;
 
 import java.awt.*;
@@ -7,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,24 +16,24 @@ import java.util.stream.Stream;
 
 public class LogFilter {
     public static List<String> filter(String file) {
-        StringBuilder text = new StringBuilder();
+        List<String> result = new ArrayList<>();
         List<String> list = new ArrayList<>();
         String elements;
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
             while ((elements = in.readLine()) != null) {
-                text.append(elements);
+                list.add(elements);
             }
-            System.out.println(text);
-            String[] token = text.toString().split(" ");
-            for (String line : token) {
-                if ("404".equals(line)) {
-                    list.add(line);
+            String[] string = list.toArray(new String[0]);
+            for (String value : string) {
+                String[] last = value.split(" ");
+                if (last[last.length - 2].equals("404")) {
+                    result.add(Arrays.toString(last));
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return list;
+        return result;
     }
 
     public static void main(String[] args) {
