f371c0080c30dcc28d63b8f057aec719475c7c62
namazbekov <82052352+namazbekov@users.noreply.github.com>
2022-01-21 20:24:31 +0300

    1. Читаем файл конфигурации [#858 #252689]

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -1,29 +1,50 @@
 package ru.job4j.io;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Config {
+
     private final String path;
-    private final Map<String, String> values = new HashMap<>();
-    private final Properties properties = new Properties();
+    private final Map<String, String> values = new HashMap<String, String>();
 
     public Config(final String path) {
         this.path = path;
     }
-// загружает файл в properties
+
     public void load() {
-        try {
-            properties.load(new BufferedReader(new FileReader(this.path)));
+        String elements;
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            while ((elements = read.readLine()) != null) {
+                String[] array = elements.split("=");
+                String first = array[0];
+                String second = array[array.length - 1];
+                if (!first.contains("#")) {
+                    values.put(first, second);
+                }
+            }
+            System.out.println(values);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-// метод value() возвращает значение ключа
+
     public String value(String key) {
-        return properties.getProperty(key);
+        String value = null;
+        for (Map.Entry<String, String> var : values.entrySet()) {
+            if (var.getKey().contains(key)) {
+                value = var.getValue();
+            }
+        }
+        return value;
     }
+
+    public static void main(String[] args) {
+        Config config = new Config("app.properties");
+        config.load();
+        System.out.println(config.value("hibernate.connection.password"));
+    }
+
 }
--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -1,29 +1,28 @@
 package ru.job4j.io;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 import static org.hamcrest.Matchers.is;
+
 import static org.junit.Assert.assertThat;
 
 public class ConfigTest {
-    @Test
-    public void whenPairWithoutComment() throws IOException {
-        String path = "c:\\projects\\job4j_design\\app.properties";
-        Config config = new Config(path);
-        config.load();
-        assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
 
-    }
     @Test
-    public void whenFindElement() throws IOException {
+    public void whenPairWithoutComment() {
         String path = "././/app.properties";
         Config config = new Config(path);
         config.load();
-        assertThat(config.value("hibernate.connection.url"), is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
+        assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
+        assertThat(config.value("surname"), is(Matchers.nullValue()));
+    }
+
+    @Test
+    public void whenPairWithoutComment2() {
+        String path = "././/add.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("animal"), is("animalPassword"));
+        assertThat(config.value("surname"), is(Matchers.nullValue()));
     }
 }
