890a35dcc14d5fefccd6b8ecb748cb7bd78e3582
namazbekov <82052352+namazbekov@users.noreply.github.com>
2022-01-23 23:34:40 +0300

    1. Читаем файл конфигурации [#858 #252689]

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 public class Config {
 
@@ -14,34 +15,40 @@ public class Config {
         this.path = path;
     }
 
-    public void load() {
+    public void load() throws IllegalArgumentException, NoSuchElementException {
         String elements;
         try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
             while ((elements = read.readLine()) != null) {
-                String[] array = elements.split("=");
-                String first = array[0];
-                String second = array[array.length - 1];
-                if (!first.contains("#")) {
-                    values.put(first, second);
+                if (elements.startsWith("#")) {
+                    continue;
                 }
+                if (elements.split("=").length > 2) {
+                    throw new IllegalArgumentException();
+                }
+                if (!elements.contains("=")) {
+                    throw new NoSuchElementException();
+                }
+                String[] array = elements.split("=");
+                if (" ".equals(array[0]) || " ".equals(array[array.length - 1])) {
+                    throw new IllegalArgumentException();
+                }
+                values.put(array[0], array[array.length - 1]);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public String value(String key) {
-        String value = null;
-        for (Map.Entry<String, String> var : values.entrySet()) {
-            if (var.getKey().contains(key)) {
-                value = var.getValue();
-            }
+    public String value(String key) throws NoSuchElementException {
+        String element = null;
+        if (values.containsKey(key)) {
+            element = values.get(key);
         }
-        return value;
+        return element;
     }
 
     public static void main(String[] args) {
-        Config config = new Config("app.properties");
+        Config config = new Config("./data/app.properties");
         config.load();
         System.out.println(config.value("hibernate.connection.password"));
     }
--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -2,27 +2,50 @@ package ru.job4j.io;
 
 import org.hamcrest.Matchers;
 import org.junit.Test;
+import java.util.NoSuchElementException;
 import static org.hamcrest.Matchers.is;
-
 import static org.junit.Assert.assertThat;
 
 public class ConfigTest {
 
     @Test
     public void whenPairWithoutComment() {
-        String path = "c:\\projects\\job4j_design\\app.properties";
+        String path = "./data//app.properties";
         Config config = new Config(path);
         config.load();
-        assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
-        assertThat(config.value("surname"), is(Matchers.nullValue()));
+        assertThat(config.value("something"), is(Matchers.nullValue()));
     }
 
     @Test
     public void whenPairWithoutComment2() {
-        String path = "c:\\projects\\job4j_design\\add.properties";
+        String path = "./data//add.properties";
         Config config = new Config(path);
         config.load();
-        assertThat(config.value("animal"), is("animalPassword"));
-        assertThat(config.value("surname"), is(Matchers.nullValue()));
+        try {
+            config.value("vjfnvj");
+        } catch (NoSuchElementException e) {
+            System.out.println("Нет такого ключа");
+        }
+    }
+
+    @Test
+    public void whenFileContainsException3() throws IllegalArgumentException {
+        String path = "./data//arr.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test
+    public void whenDontFindElement() throws NoSuchElementException {
+        String path = "./data//aff.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test
+    public void whenNoLegalElement() throws IllegalArgumentException {
+        String path = "./data//acc.properties";
+        Config config = new Config(path);
+        config.load();
     }
 }
