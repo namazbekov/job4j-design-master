16395ae2de0646c2c9e2ee53fb2a45889b845af7
namazbekov <82052352+namazbekov@users.noreply.github.com>
2022-01-25 17:19:09 +0300

    2. Анализ доступности сервера. [#859 #253576]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Analizy {
-    @SuppressWarnings("checkstyle:SimplifyBooleanExpression")
+
     public static void unavailable(String source) {
         String read;
         StringBuilder stringBuilder = new StringBuilder();
@@ -17,9 +17,11 @@ public class Analizy {
                     stringBuilder.append(array[1]).append(";");
                     isWorks = false;
                 }
+
                 if ("300".equals(array[0]) || "200".equals(array[0]) && !isWorks) {
                     stringBuilder.append(array[1]).append(";");
                     isWorks = true;
+                    stringBuilder.append(System.lineSeparator());
                 }
             }
             System.out.println(stringBuilder);
