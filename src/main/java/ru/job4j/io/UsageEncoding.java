ac588f27a54178916843ff9da74a94610430e89f
user1 <dastan.namazbekov@mail.ru>
2022-02-17 23:32:59 +0300

    6. Кодировка. [#862 #266117]

---
+++ src/main/java/ru/job4j/io/UsageEncoding.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class UsageEncoding {
+
+    public String readFile(String path) {
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            br.lines().forEach(builder::append);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+
+    public void writeDataInFile(String path, String data) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(path, true))) {
+            pw.println(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String path = "./data/text.txt";
+        UsageEncoding encoding = new UsageEncoding();
+        String s = encoding.readFile(path);
+        String[] array = s.split(";");
+        System.out.println("Данные из файла: ");
+        for (String str : array) {
+            System.out.println(str);
+        }
+    }
+}
