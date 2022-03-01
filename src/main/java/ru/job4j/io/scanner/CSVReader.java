aab9536006018d5a652ba81c4733bac95a0f1bcd
user1 <dastan.namazbekov@mail.ru>
2022-03-01 23:56:28 +0300

    7. Scanner [#504791 #269196]

--- src/main/java/ru/job4j/io/scanner/CSVReader.java
+++ src/main/java/ru/job4j/io/scanner/CSVReader.java
@@ -1,47 +1,51 @@
 package ru.job4j.io.scanner;
 
-import com.sun.jdi.request.StepRequest;
 import ru.job4j.io.ArgsName;
 import java.io.*;
 import java.util.*;
 
 public class CSVReader {
 
-    static List<String> result = new ArrayList<>();
+    static StringBuilder stringBuilder = new StringBuilder();
 
     public static void handle(ArgsName argsName) {
         String[] filter = argsName.get("filter").split(",");
         String path = argsName.get("path");
         String delimiter = argsName.get("delimiter");
         String out = argsName.get("out");
-        int indexName = 0;
-        int indexAge = 0;
+        List<String> collectIndex = new ArrayList<>();
         try (Scanner scanner = new Scanner(new File(path))) {
             String firstRow = scanner.nextLine();
             List<String> list = List.of(firstRow.split(";"));
-            if (list.contains(filter[0]) && list.contains(filter[1])) {
-                indexName = list.indexOf(filter[0]);
-                indexAge = list.indexOf(filter[1]);
-                result.add(filter[0] + ";" + filter[1]);
+            for (String element : list) {
+                for (String value : filter) {
+                    if (element.equals(value)) {
+                        collectIndex.add(String.valueOf(list.indexOf(value)));
+                    }
+                }
             }
             while (scanner.hasNextLine()) {
                 String nextLine = scanner.nextLine();
                 List<String> list1 = List.of(nextLine.split(delimiter));
-                String name = list1.get(indexName);
-                var age = list1.get(indexAge);
-                result.add(name + ";" + age);
-            }
-            if (out.equals("stdout")) {
-                for (String array : result) {
-                    System.out.println(array);
+                for (int i = 0; i < list1.size(); i ++) {
+                    for (int j = 0; j < collectIndex.size(); j++) {
+                        int element = Integer.parseInt(collectIndex.get(j));
+                        if (i == element) {
+                            stringBuilder.append(list1.get(i)).append(" ");
+                        }
+                    }
                 }
+                stringBuilder.append(System.lineSeparator());
+            }
+
+            if ("stdout".equals(out)) {
+                System.out.println(stringBuilder);
             } else {
                 File file = new File(out);
                 try (FileWriter fileWriter = new FileWriter(file)) {
-                    for (String array : result) {
-                        fileWriter.write(array);
+                        fileWriter.write(String.valueOf(stringBuilder));
                         fileWriter.write(System.lineSeparator());
-                    }
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -50,9 +54,7 @@ public class CSVReader {
             e.printStackTrace();
         }
     }
-
-
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void checkArguments(String[] args) {
         if (args.length != 4) {
             throw new IllegalArgumentException("number of arguments isn`t correct");
         }
@@ -60,14 +62,14 @@ public class CSVReader {
         if (file.isFile()) {
             throw new IllegalArgumentException("path of source isn`t correct");
         }
-        String[] filter = args[3].split(",");
-        if (filter.length != 2) {
-            throw new IllegalArgumentException("filter isn`t correct");
-        }
         String delimiter = args[1];
         if (!delimiter.endsWith(";")) {
             throw new IllegalArgumentException("delimiter isn`t correct");
         }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        checkArguments(args);
         handle(ArgsName.of(new String[] {
                 args[0], args[1], args[2], args[3]
         }));
