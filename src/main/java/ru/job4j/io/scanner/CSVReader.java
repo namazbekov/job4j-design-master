be9fee3ce420f2ce54b1fe4cbefeae4b96c99389
user1 <dastan.namazbekov@mail.ru>
2022-03-06 14:26:45 +0300

    7. Scanner [#504791 #269196]

--- src/main/java/ru/job4j/io/scanner/CSVReader.java
+++ src/main/java/ru/job4j/io/scanner/CSVReader.java
@@ -16,14 +16,16 @@ public class CSVReader {
         List<String> collectIndex = new ArrayList<>();
         try (Scanner scanner = new Scanner(new File(path))) {
             String firstRow = scanner.nextLine();
-            List<String> list = List.of(firstRow.split(";"));
+            List<String> list = List.of(firstRow.split(delimiter));
             for (String element : list) {
                 for (String value : filter) {
                     if (element.equals(value)) {
                         collectIndex.add(String.valueOf(list.indexOf(value)));
+                        stringBuilder.append(element).append(delimiter);
                     }
                 }
             }
+            stringBuilder.append(System.lineSeparator());
             while (scanner.hasNextLine()) {
                 String nextLine = scanner.nextLine();
                 List<String> list1 = List.of(nextLine.split(delimiter));
@@ -31,7 +33,7 @@ public class CSVReader {
                     for (int j = 0; j < collectIndex.size(); j++) {
                         int element = Integer.parseInt(collectIndex.get(j));
                         if (i == element) {
-                            stringBuilder.append(list1.get(i)).append(" ");
+                            stringBuilder.append(list1.get(i)).append(delimiter);
                         }
                     }
                 }
@@ -55,24 +57,23 @@ public class CSVReader {
         }
     }
     public static void checkArguments(String[] args) {
+        ArgsName argsName = ArgsName.of(args);
+        File file = new File(argsName.get("path"));
+        String delimiter = argsName.get("delimiter");
         if (args.length != 4) {
             throw new IllegalArgumentException("number of arguments isn`t correct");
         }
-        File file = new File(args[0]);
-        if (file.isFile()) {
+        if (!file.isFile()) {
             throw new IllegalArgumentException("path of source isn`t correct");
         }
-        String delimiter = args[1];
-        if (!delimiter.endsWith(";")) {
+        if (!";".equals(delimiter)) {
             throw new IllegalArgumentException("delimiter isn`t correct");
         }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
         checkArguments(args);
-        handle(ArgsName.of(new String[] {
-                args[0], args[1], args[2], args[3]
-        }));
+        handle(ArgsName.of(args));
 
     }
 }
