8ee05449db8fa11690715d8d306bb708121fecbf
user1 <dastan.namazbekov@mail.ru>
2022-03-12 14:18:29 +0300

    3. Бот [#7921 #273751]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,6 +3,7 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLOutput;
 import java.util.List;
 
 public class EchoServer {
@@ -14,15 +15,19 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write("Hello my friend \r\n\r\n".getBytes());
                     String str = in.readLine();
                     if (str != null && !str.isEmpty()) {
                         String[] array = str.split("=");
                         List<String> list = List.of(array[array.length - 1].split(" "));
-                        if (list.contains("Bye")) {
+                        if (list.contains("Hello")) {
+                            System.out.println("Hello my friend");
+                        } else if (list.contains("Exit")) {
                             System.out.println("Good Bye");
                             server.close();
+                        }else {
+                            System.out.println(list.get(0));
                         }
-                        System.out.println(list.get(0));
                     }
                     out.flush();
                 }
