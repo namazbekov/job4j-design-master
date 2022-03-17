0e85281a3a9092a036f3234680e9c5096235837b
user1 <dastan.namazbekov@mail.ru>
2022-03-17 17:35:40 +0300

    3. Формат XML [#313165 #276453]

--- src/main/java/ru/job4j/serialization/xml/Main.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.serialization.xml;
-
-public class Main {
-    public static void main(String[] args) {
-        final Person person = new Person(false, 30,
-                new Contact("11-111"), "Worker", "Married");
-    }
-}
