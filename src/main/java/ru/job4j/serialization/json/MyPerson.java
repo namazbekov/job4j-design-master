efe72b0f892998ed242ceb221ca61e84780f602f
user1 <dastan.namazbekov@mail.ru>
2022-03-21 09:53:04 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/serialization/json/MyPerson.java
+++ src/main/java/ru/job4j/serialization/json/MyPerson.java
@@ -1,5 +1,7 @@
 package ru.job4j.serialization.json;
 
+import org.json.JSONObject;
+
 import java.util.Arrays;
 
 public class MyPerson {
@@ -9,12 +11,32 @@ public class MyPerson {
     final Relative relative;
     final String[] whereHeLives;
 
-    public MyPerson(String name, int age, boolean sex, Relative relative, String[] whereHeLives) {
+    public MyPerson(String name, int age, boolean sex, Relative relative, String... whereHeLives) {
         this.name = name;
         this.age = age;
         this.sex = sex;
         this.relative = relative;
         this.whereHeLives = whereHeLives;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public Relative getRelative() {
+        return relative;
+    }
+
+    public String[] getWhereHeLives() {
+        return whereHeLives;
     }
 
     @Override
@@ -26,5 +48,20 @@ public class MyPerson {
                 + ", relative=" + relative
                 + ", whereHeLives=" + Arrays.toString(whereHeLives)
                 + '}';
+    }
+
+    public static void main(String[] args) {
+        final MyPerson person =
+                new MyPerson("Nikita", 32, true,
+                        new Relative("Vitalik", "Nastia"), "Moscow", "Piter");
+        JSONObject json = new JSONObject();
+        json.put("name", person.name);
+        json.put("age", person.age);
+        json.put("sex", person.isSex());
+        json.put("relative", person.relative);
+        json.put("whereHeLives", person.whereHeLives);
+        System.out.println(json);
+
+        System.out.println(new JSONObject(person));
     }
 }
