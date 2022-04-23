e1138340301a7fa0304a90782ac680f54127a634
user1 <dastan.namazbekov@mail.ru>
2022-04-23 20:33:53 +0300

    0. Что такое база данных. [#6860 #290460]

--- filesSql/script.sql
+++ filesSql/script.sql
@@ -1,9 +1,10 @@
 create table human(
 	id serial primary key,
 	name varchar (255),
-	area text
+	area text,
+	Age INTEGER
 );
-insert into human(name, area) values('Michael', 'USA');
+insert into human(name, area, Age) values('Michael', 'USA', '34');
 select * from human;
 update human set name = 'Piter';
 select * from human;
