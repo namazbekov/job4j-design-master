080bac2794d307c16043e4bb3c7b9db9bc6bc7fa
user1 <dastan.namazbekov@mail.ru>
2022-05-25 18:07:49 +0300

    2. Группировка и агрегатные функции [#373618 #304020]

--- filesSql/group.sql
+++ filesSql/group.sql
@@ -16,21 +16,17 @@ create table devices_people(
 );
 insert into devices (name, price)
 values ('Samsung', 50000), ('MI', 12000), ('Iphone', 130000);
-insert into people (name) values ('Dima');
-insert into people (name) values ('Pashya');
-insert into people (name) values ('Vanya');
-insert into devices_people (devices_id, people_id) values (1, 1);
-insert into devices_people (devices_id, people_id) values (2, 1);
-insert into devices_people (devices_id, people_id) values (3, 2);
-insert into devices_people (devices_id, people_id) values (1, 2);
-insert into devices_people (devices_id, people_id) values (2, 3);
-insert into devices_people (devices_id, people_id) values (1, 3);
-insert into devices_people (devices_id, people_id) values (3, 3);
+insert into people (name) values ('Dima'), ('Pashya'), ('Vanya');
 
-select avg(d.price), p.name, dp.devices_id
+insert into devices_people (devices_id, people_id) values (1, 1), (2, 1), (3, 1);
+insert into devices_people (devices_id, people_id) values (2, 2), (2, 2), (2, 2);
+insert into devices_people (devices_id, people_id) values (1, 3), (1, 3), (1, 3);
+select * from devices_people
+
+select p.name, dp.devices_id, avg(d.price)
 from devices as d
 join devices_people as dp
 on d.id = dp.devices_id
 join people as p
-on p.id = dp.devices_id
+on p.id = dp.people_id
 group by p.name, dp.devices_id
