21c78bfd80331e566b8dca8d1321b37e995f173e
user1 <dastan.namazbekov@mail.ru>
2022-06-02 23:08:57 +0300

    2. Группировка и агрегатные функции [#373618 #304020]

--- filesSql/group.sql
+++ filesSql/group.sql
@@ -21,7 +21,8 @@ insert into people (name) values ('Dima'), ('Pashya'), ('Vanya');
 insert into devices_people (devices_id, people_id) values (1, 1), (2, 1), (3, 1);
 insert into devices_people (devices_id, people_id) values (2, 2), (2, 2), (2, 2);
 insert into devices_people (devices_id, people_id) values (1, 3), (1, 3), (1, 3);
-select * from devices_people
+
+select avg(price) from devices;
 
 select p.name, avg(d.price)
 from devices_people
@@ -30,3 +31,5 @@ on d.id = devices_id
 join people as p
 on p.id = people_id
 group by p.name
+having avg(d.price) > 5000;
+
