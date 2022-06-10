f9b29689764cc8f5c1789e4717743f997dc47bd5
user1 <dastan.namazbekov@mail.ru>
2022-06-10 22:07:04 +0600

    3. Фильтры [#1732 #310329]

--- filesSql/filter.sql
+++ filesSql/filter.sql
@@ -14,36 +14,37 @@ insert into type (name) values ('Сыр');
 insert into type (name) values ('Мясо');
 insert into type (name) values ('Молоко');
 
-insert into product (name, type_id, expried_date, price) 
+insert into product (name, type_id, expried_date, price)
 values ('Хохлфнд', 1, date '12-06-2022', 150);
-insert into product (name, type_id, expried_date, price) 
+insert into product (name, type_id, expried_date, price)
 values ('Голандский сыр', 1, date '24-06-2022', 600);
-insert into product (name, type_id, expried_date, price) 
+insert into product (name, type_id, expried_date, price)
 values ('Грузинский сыр', 1, date '15-06-2022', 350);
-insert into product (name, type_id, expried_date, price) 
+insert into product (name, type_id, expried_date, price)
 values ('Черкизовские сосиски', 2, date '18-06-2022', 325);
-insert into product (name, type_id, expried_date, price) 
+insert into product (name, type_id, expried_date, price)
 values ('Краковская говядина', 2, date '19-06-2022', 450);
-insert into product (name, type_id, expried_date, price) 
+insert into product (name, type_id, expried_date, price)
 values ('Ставрополькая курица', 2, date '20-06-2022', 140);
-insert into product (name, type_id, expried_date, price) 
+insert into product (name, type_id, expried_date, price)
 values ('Вкуснотеевское молоко', 3, date '21-06-2022', 75);
-insert into product (name, type_id, expried_date, price) 
+insert into product (name, type_id, expried_date, price)
 values ('ЭкониваАгро молоко', 3, date '12-07-2022', 65);
-insert into product (name, type_id, expried_date, price) 
+insert into product (name, type_id, expried_date, price)
 values ('Хохольское молоко', 3, date '23-06-2022', 80);
+insert into product (name, type_id, expried_date, price)
+values ('Молочный чай', 3, date '23-06-2024', 600);
 
-select * from product 
+select * from product
 join type t on product.type_id = t.id where t.name = 'Сыр';
 
 select * from product where name like '%ороженно%';
 
 select expried_date < current_date from product;
 
-select name, max(price) from product
-group by name;
+select name, price from product where price = (select max(price) from product);
 
-select * from product 
+select * from product
 join type t on product.type_id = t.id
 where t.name = 'Сыр' or t.name = 'Молоко';
 
