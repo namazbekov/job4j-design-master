319851756213cb41db8438e74a1146244dd36eb7
user1 <dastan.namazbekov@mail.ru>
2023-06-15 21:14:59 +0600

    5.0. Буферизированные потоки [#504979]

---
+++ src/main/java/ru/job4j/io/Buffered.java
@@ -0,0 +1,15 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Buffered {
+    public static void main(String[] args) {
+        try (BufferedReader in = new BufferedReader(new FileReader("data/newData.txt"));
+             BufferedWriter out = new BufferedWriter(new FileWriter("data/output.txt", true) {
+             })) {
+            out.write(in.readLine());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
